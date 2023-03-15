b5f7ff17342b57e0046f72040ae68d4e79fc99d8
Nataliia Shkarupeta <natalinardi.kh@gmail.com>
2023-03-15 09:47:53 +0200

    style for body

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,28 +3,23 @@ import { Navigation } from './Navigation/Navigation';
 import { HomePage } from 'pages/HomePage/HomePage';
 import { ListPoints } from 'pages/ListPointsPage/ListPointsPage';
 import { Helmet } from 'react-helmet';
-import { BasicWrap } from './defaultSlyle';
-
+// import { BasicWrap } from './defaultSlyle';
 
 export const App = () => {
-
-
   return (
     <>
-      <BasicWrap>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>Під Контролем</title>
-          <link rel="canonical" href="http://mysite.com/example" />
-          <meta name="description" content="Отримання інформації по запитам" />
-        </Helmet>
-        <Routes>
-          <Route path="/" element={<Navigation />}>
-            <Route index element={<HomePage />} />
-            <Route path="branches" element={<ListPoints />} />
-          </Route>
-        </Routes>
-      </BasicWrap>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Під Контролем</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="Отримання інформації по запитам" />
+      </Helmet>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<HomePage />} />
+          <Route path="branches" element={<ListPoints />} />
+        </Route>
+      </Routes>
     </>
   );
 };
--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -6,10 +6,9 @@ import styled from 'styled-components';
    padding-left: ${p => p.theme.space[3]}px;
    flex: 0 0 auto;
    height: ${p => p.theme.sizes[7]}px;
-   background-color: 1px 52px 1px rgba(132, 113, 142, 0.5);
-   border-top: ${p => p.theme.borders.normal};
+   /* border-top: ${p => p.theme.borders.normal}; */
    box-shadow: 1px 0px 8px rgba(238, 238, 238, 0.91);
-   border-color: ${p => p.theme.colors.muted};
+   /* border-color: ${p => p.theme.colors.muted}; */
    @media screen and (${p => p.theme.media.large}) {
      height: ${p => p.theme.sizes[5]}px;
    }
@@ -17,7 +16,7 @@ import styled from 'styled-components';
      margin-top: ${p => p.theme.space[5]}px;
    }
    @media screen and (min-width: 961px) {
-     margin-top: ${p => p.theme.space[6]}px;
+     margin-top: ${p => p.theme.space[7]}px;
    }
  `;
 
